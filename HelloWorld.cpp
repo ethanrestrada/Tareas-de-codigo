90e79a1b6df3a8b4dc9c97788b480d9fb89ba665
Ethan Ruíz Estrada <ethanruiz25@meso.edu>
2023-02-15 07:35:22 -0600

    first project

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "Hello World!\n";
+    cout << "Ethan Eduardo Ruiz Estrada\n";
+    cout << "Carnet: 202308079\n";
+    cout << "Escogi la carrera porque me gusta la programacion\n";
+
+    system("pause");
+    return 0;
+}
